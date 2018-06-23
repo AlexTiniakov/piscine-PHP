351db94649d6ec944f16edd824e867b99d3a96bc
Oleksandr TINIAKOV <otiniako@e2r1p4.unit.ua>
2018-06-23 22:11:55 +0300

    23.06/1

--- day04/ex00/index.php
+++ day04/ex00/index.php
@@ -9,7 +9,7 @@
 <form action="index.php" method="GET">
 	Username: <input type="text" name="login" value="<?php echo $_SESSION['login']; ?>"/>
 	<br />
-	Password: <input type="passwd" name="passwd" value="<?php echo $_SESSION['passwd']; ?>"/>
+	Password: <input type="password" name="passwd" value="<?php echo $_SESSION['passwd']; ?>"/>
 	<br />
 	<input type="submit" name="submit" value="OK" />
 </form>
